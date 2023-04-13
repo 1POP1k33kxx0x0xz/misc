f862d80da5fffe0219f1c73a386b13a5d0ffd776
1POP1k33kxx0x0xz <86627910+1POP1k33kxx0x0xz@users.noreply.github.com>
2023-04-13 10:33:56 -0400

    Update wins.lua

--- wins.lua
+++ wins.lua
@@ -52,7 +52,7 @@ local function addWin()
 end
 
 local function onKick()
-    teleportService:TeleportAsync(game.PlaceId, {localPlayer})
+    teleportService:Teleport(game.PlaceId)
 end
 
 for _,child in ipairs(workspace:GetChildren()) do
